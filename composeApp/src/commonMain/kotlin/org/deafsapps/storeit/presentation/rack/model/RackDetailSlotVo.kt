41465496d90bc16c08f7fb7acce1b97a5c32078e
Pablo L. Sordo <pablodeafsapps@gmail.com>
2026-03-16 13:15:18 +0100

    refactor: rename `RackDetailSlotView` to `RackDetailSlotVo` and add presentation mappers
    - Renamed `RackDetailSlotView` to `RackDetailSlotVo` to align with Value Object naming conventions in the presentation layer.
    - Added `RackMapper.kt` containing extension functions to map `ShelfSlot` domain models to `RackDetailSlotVo`.
    - Updated the data class to maintain its `@Stable` property and coordinate-based fields.

--- composeApp/src/commonMain/kotlin/org/deafsapps/storeit/presentation/rack/model/RackDetailSlotVo.kt
+++ composeApp/src/commonMain/kotlin/org/deafsapps/storeit/presentation/rack/model/RackDetailSlotVo.kt
@@ -3,7 +3,7 @@ package org.deafsapps.storeit.presentation.rack.model
 import androidx.compose.runtime.Stable
 
 @Stable
-data class RackDetailSlotView(
+data class RackDetailSlotVo(
     val id: String,
     val xRel: Float,
     val yRel: Float,
