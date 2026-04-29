847c0ec1e6ac67692db42e9c791f53417314220a
Pablo L. Sordo <pablodeafsapps@gmail.com>
2026-04-29 23:03:52 +0200

    feat: integrate Firebase dependencies and refactor initialization for iOS
    - Integrated `FirebaseAuth`, `FirebaseFirestore`, and `FirebaseStorage` into the iOS project via CocoaPods.
    - Added `FirebaseAnalytics` and `FirebaseAnalyticsCore` dependencies using Swift Package Manager.
    - Refactored `iOSApp.swift` to handle Firebase configuration within an `AppDelegate` using `@UIApplicationDelegateAdaptor` for improved lifecycle management.
    - Updated the Xcode project configuration with necessary build phases for CocoaPods, including manifest locking and framework embedding.
    - Introduced `Podfile.lock` to ensure consistent dependency versions across environments.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,18 +1,22 @@
 import SwiftUI
 import Shared
-#if canImport(FirebaseCore)
 import FirebaseCore
-#endif
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct iOSApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     init() {
-        #if canImport(FirebaseCore)
-        if FirebaseApp.app() == nil {
-            FirebaseApp.configure()
-        }
-        #endif
-
         KoinInitKt.doInitKoinIos()
     }
 
